777edc941a1ac3b3a55990d6c2b737d8c41f6e01
Oleksandr Miroshnychenko <almir.php.developer@gmail.com>
2018-08-09 16:31:23 +0300

    struct_modules

---
+++ app/Modules/ModulesServiceProvider.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlMir
+ * Date: 09.08.2018
+ * Time: 12:06
+ */
---
+++ app/Modules/Site/Views/layouts/main_site_layout.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlMir
+ * Date: 09.08.2018
+ * Time: 16:07
+ */
